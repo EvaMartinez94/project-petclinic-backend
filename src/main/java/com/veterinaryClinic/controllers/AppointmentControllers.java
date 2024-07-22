d68ac4ade0fe921efa18c4c835ab1fb6c1bea238
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-07-22 19:39:19 +0200

    update imports

--- src/main/java/com/veterinaryClinic/controllers/AppointmentControllers.java
+++ src/main/java/com/veterinaryClinic/controllers/AppointmentControllers.java
@@ -5,8 +5,6 @@ import com.veterinaryClinic.services.AppointmentServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-
 @RestController
 @RequestMapping("/api/vc")
 @CrossOrigin(origins = "*")
