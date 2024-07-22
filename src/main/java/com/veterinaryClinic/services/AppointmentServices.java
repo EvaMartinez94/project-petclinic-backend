60dcf9ecd1e96e2fa88a3539e75582108b9817a8
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-22 15:11:59 +0200

    annotation service

--- src/main/java/com/veterinaryClinic/services/AppointmentServices.java
+++ src/main/java/com/veterinaryClinic/services/AppointmentServices.java
@@ -3,9 +3,11 @@ package com.veterinaryClinic.services;
 import com.veterinaryClinic.models.Appointment;
 import com.veterinaryClinic.repositories.IAppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Optional;
+@Service
 
 public class AppointmentServices {
     @Autowired
