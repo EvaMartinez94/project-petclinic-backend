97bcb0e36167a0080105c1e0ee0518070c6309de
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-07-22 14:40:21 +0200

    feature delete appointments added

--- src/main/java/com/veterinaryClinic/controllers/AppointmentControllers.java
+++ src/main/java/com/veterinaryClinic/controllers/AppointmentControllers.java
@@ -1,3 +1,19 @@
 package com.veterinaryClinic.controllers;
 
-public class AppointmentControllers {}
+import com.veterinaryClinic.services.AppointmentServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/vc")
+@CrossOrigin(origins = "*")
+public class AppointmentControllers {
+
+    @Autowired
+    AppointmentServices appointmentServices;
+
+    @DeleteMapping(path = "appointment/{id}")
+    public void deleteAppointment(@PathVariable Integer id){
+        appointmentServices.deleteAppointment(id);
+    }
+}
--- src/main/java/com/veterinaryClinic/services/AppointmentServices.java
+++ src/main/java/com/veterinaryClinic/services/AppointmentServices.java
@@ -1,3 +1,16 @@
 package com.veterinaryClinic.services;
 
-public class AppointmentServices {}
+import com.veterinaryClinic.repositories.IAppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentServices {
+
+    @Autowired
+    IAppointmentRepository iAppointmentRepository;
+
+    public void deleteAppointment(Integer id){
+        iAppointmentRepository.deleteById(id);
+    }
+}
