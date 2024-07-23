5406dd290e3967bd8adbc14c298dc39dad31b191
jmat732 <jmatisamuel@gmail.com>
2024-07-23 08:37:27 +0200

    create Patietn model y post create

--- src/main/java/com/veterinaryClinic/controllers/PatientControllers.java
+++ src/main/java/com/veterinaryClinic/controllers/PatientControllers.java
@@ -1,3 +1,31 @@
 package com.veterinaryClinic.controllers;
 
-public class PatientControllers {}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.veterinaryClinic.models.Patient;
+import com.veterinaryClinic.services.PatientServices;
+
+
+@RestController
+@RequestMapping("/vc/patient")
+public class PatientControllers {
+    
+
+    @Autowired
+    private PatientServices patientService;
+
+// CREATE
+    @PostMapping
+    public ResponseEntity<Patient> createPatient(@RequestBody Patient newPatient) {
+        Patient patient = patientService.cretePatient(newPatient);
+        return new ResponseEntity<>(patient, HttpStatus.CREATED);
+    }
+
+
+}
--- src/main/java/com/veterinaryClinic/models/Patient.java
+++ src/main/java/com/veterinaryClinic/models/Patient.java
@@ -1,3 +1,69 @@
 package com.veterinaryClinic.models;
 
-public class Patient {}
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.NaturalId;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "patient", indexes = {
+    @Index(name = "idx_patient_identificationNumber", columnList = "identificationNumber"),
+    @Index(name = "idx_patient_tutorName", columnList = "tutorName"),
+    @Index(name = "idx_patient_identification_tutor", columnList = "identificationNumber, tutorName")
+})
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NaturalId
+    @Column(name = "identificationNumber", nullable = false, unique = true)
+    private Long identificationNumber;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "race")
+    private String race;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "tutorName", nullable = false)
+    private String tutorName;
+
+    @Column(name = "tutorPhone", nullable = false)
+    private String tutorPhone;
+
+    @Column(name = "treatment")
+    private String treatment;
+
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Appointment> appointments = new ArrayList<>();//falta el modelo que me lo baje de
+
+}
+
