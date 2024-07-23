bb29789f6264fe349b4b2bffd4785f416418730d
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-23 13:59:38 +0200

    merge branches appointments

--- src/main/java/com/veterinaryClinic/services/AppointmentServices.java
+++ src/main/java/com/veterinaryClinic/services/AppointmentServices.java
@@ -4,38 +4,33 @@ import com.veterinaryClinic.models.Appointment;
 import com.veterinaryClinic.repositories.IAppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
 public class AppointmentServices {
 
-    @Autowired
-    IAppointmentRepository iAppointmentRepository;
+  @Autowired IAppointmentRepository iAppointmentRepository;
 
-    public void updateAppointment(Appointment appointment, Integer id){
-        appointment.setId(id);
-        iAppointmentRepository.save(appointment);
-    }
+  public void updateAppointment(Appointment appointment, Integer id) {
+    appointment.setId(id);
+    iAppointmentRepository.save(appointment);
+  }
 
-    public Appointment createAppointment (Appointment newAppointment){
-        return iAppointmentRepository.save(newAppointment);
+  public Appointment createAppointment(Appointment newAppointment) {
+    return iAppointmentRepository.save(newAppointment);
+  }
 
-    public ArrayList<Appointment> getAllAppointment(){
-        return (ArrayList<Appointment>) iAppointmentRepository.findAll();
-    }
+  public ArrayList<Appointment> getAllAppointment() {
+    return (ArrayList<Appointment>) iAppointmentRepository.findAll();
+  }
 
-    public Optional<Appointment> getAppointmentId(int id){
-        Appointment appointment = iAppointmentRepository.findById(id).orElseThrow();
-        return Optional.of(appointment);
-    }
+  public Optional<Appointment> getAppointmentId(int id) {
+    Appointment appointment = iAppointmentRepository.findById(id).orElseThrow();
+    return Optional.of(appointment);
+  }
 
-}
-
-    public void deleteAppointment(Integer id){
-        iAppointmentRepository.deleteById(id);
-    }
+  public void deleteAppointment(Integer id) {
+    iAppointmentRepository.deleteById(id);
+  }
 }
