e98ed5da485e34e88bc338dd427493d8fa58ebe7
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-23 12:44:08 +0200

    merge branch post

--- src/main/java/com/veterinaryClinic/controllers/AppointmentControllers.java
+++ src/main/java/com/veterinaryClinic/controllers/AppointmentControllers.java
@@ -1,7 +1,7 @@
 package com.veterinaryClinic.controllers;
 
 import com.veterinaryClinic.models.Appointment;
-import com.veterinaryClinic.services.AppointmentServices;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
