f25842bc624c342d1138211a82757f47b895be6d
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-24 11:39:36 +0200

    annotation service

--- src/main/java/com/veterinaryClinic/controllers/PatientControllers.java
+++ src/main/java/com/veterinaryClinic/controllers/PatientControllers.java
@@ -12,35 +12,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.veterinaryClinic.models.Patient;
 import com.veterinaryClinic.services.PatientServices;
-
 
 @RestController
 @RequestMapping("/api/vc/")
 public class PatientControllers {
-    
-    @Autowired
-    private PatientServices patientService;
 
-    @GetMapping(path = "/patient")
-    public List<Patient> getAllPatients(){
-        return patientService.getAllPatients();
-    }
+  @Autowired private PatientServices patientService;
 
-    @GetMapping(path = "patient/{id}")
-    public Optional<Patient> getPatientbyId(@PathVariable Long id){
-        return patientService.getPatientbyId(id);
-    }
+  @GetMapping(path = "/patient")
+  public List<Patient> getAllPatients() {
+    return patientService.getAllPatients();
+  }
 
-    @GetMapping(path = "/patient/{tutorName}")
-    public List<Patient> getByTutorName(String tutorName){
-        return patientService.getByTutorName(tutorName);
-    }
+  @GetMapping(path = "patient/{id}")
+  public Optional<Patient> getPatientbyId(@PathVariable Long id) {
+    return patientService.getPatientbyId(id);
+  }
 
-    @GetMapping(path = "/patient/{identificationNumber}")
-    public List<Patient> getByIdentificationNumber(Long identificationNumber){
-        return patientService.getByIdentificationNumber(identificationNumber);
-    }
+  @GetMapping(path = "/patient/{tutorName}")
+  public List<Patient> getByTutorName(String tutorName) {
+    return patientService.getByTutorName(tutorName);
+  }
+
+  @GetMapping(path = "/patient/{identificationNumber}")
+  public List<Patient> getByIdentificationNumber(Long identificationNumber) {
+    return patientService.getByIdentificationNumber(identificationNumber);
+  }
 }
--- src/main/java/com/veterinaryClinic/services/PatientServices.java
+++ src/main/java/com/veterinaryClinic/services/PatientServices.java
@@ -8,28 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.veterinaryClinic.models.Patient;
 import com.veterinaryClinic.repositories.IPatientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 
 public class PatientServices {
 
+  @Autowired IPatientRepository iPatientRepository;
 
+  public List<Patient> getAllPatients() {
+    return (List<Patient>) iPatientRepository.findAll();
+  }
 
-     @Autowired IPatientRepository iPatientRepository;
+  public Optional<Patient> getPatientbyId(Long id) {
+    Patient patient = iPatientRepository.findById(id).orElseThrow();
+    return Optional.of(patient);
+  }
 
-     public List<Patient> getAllPatients(){
-        return (List<Patient>) iPatientRepository.findAll();
-     }
+  public List<Patient> getByTutorName(String tutorName) {
+    return (List<Patient>) iPatientRepository.findByTutorName(tutorName);
+  }
 
-     public Optional<Patient> getPatientbyId(Long id){
-        Patient patient = iPatientRepository.findById(id).orElseThrow();
-        return Optional.of(patient);
-     }
-
-     public List<Patient> getByTutorName(String tutorName){
-        return (List<Patient>) iPatientRepository.findByTutorName(tutorName);
-     }
-
-     public List<Patient> getByIdentificationNumber(Long identificationNumber){
-        return (List<Patient>) iPatientRepository.findByIdentificationNumber(identificationNumber);
-     }
-
+  public List<Patient> getByIdentificationNumber(Long identificationNumber) {
+    return (List<Patient>) iPatientRepository.findByIdentificationNumber(identificationNumber);
+  }
 }
