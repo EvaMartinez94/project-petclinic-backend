a7effa8f038630bb0557a5cc24c17a67b51148c9
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-07-24 14:39:04 +0200

    objects for test

--- src/test/java/com/veterinaryClinic/services/AppointmentServicesTest.java
+++ src/test/java/com/veterinaryClinic/services/AppointmentServicesTest.java
@@ -1,3 +1,44 @@
 package com.veterinaryClinic.services;
 
-public class AppointmentServicesTest {}
+import com.veterinaryClinic.models.Appointment;
+import com.veterinaryClinic.repositories.IAppointmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class AppointmentServicesTest {
+
+  @Mock private IAppointmentRepository iAppointmentRepository;
+  @InjectMocks private AppointmentServices appointmentServices;
+
+  private Appointment appointmentDuque;
+  private Appointment appointmentKoda;
+  private ArrayList<Appointment> appointmentList = new ArrayList<>();
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+
+    appointmentDuque = new Appointment();
+    appointmentDuque.setDate(LocalDate.of(2024, 10, 10));
+    appointmentDuque.setTime(LocalTime.of(19, 30));
+    appointmentDuque.setPatient("Duque");
+    appointmentDuque.setEmergency(false);
+    appointmentDuque.setReason("lele pancha");
+    appointmentDuque.setPast(false);
+
+    appointmentKoda = new Appointment();
+    appointmentKoda.setDate(LocalDate.of(2024, 04, 29));
+    appointmentKoda.setTime(LocalTime.of(12, 10));
+    appointmentKoda.setPatient("Koda");
+    appointmentKoda.setEmergency(true);
+    appointmentKoda.setReason("caca explosiva");
+    appointmentKoda.setPast(true);
+  }
+
+}
