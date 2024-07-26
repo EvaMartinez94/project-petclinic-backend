007b484d0dae27fc7ad44817839a8dfe47aea9de
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-07-26 08:26:01 +0200

    added imports to tests

--- src/test/java/com/veterinaryClinic/controllers/PatientControllersTest.java
+++ src/test/java/com/veterinaryClinic/controllers/PatientControllersTest.java
@@ -1,3 +1,23 @@
 package com.veterinaryClinic.controllers;
 
-public class PatientControllersTest {}
+import com.veterinaryClinic.models.Appointment;
+import com.veterinaryClinic.models.Patient;
+import com.veterinaryClinic.repositories.IAppointmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+public class PatientControllersTest {
+
+    
+}
