a982630b7a0fb25db5f18c5176fa6ae9ca65aefc
José Manuel Alonso Tirado <132337570+jmatisam@users.noreply.github.com>
2024-07-30 09:07:42 +0200

    add path a getall

--- src/main/java/com/veterinaryClinic/controllers/PatientControllers.java
+++ src/main/java/com/veterinaryClinic/controllers/PatientControllers.java
@@ -31,7 +31,7 @@ public class PatientControllers {
       return new ResponseEntity<>(patient, HttpStatus.CREATED);
     }
     
-    @GetMapping
+    @GetMapping(path = "/patient")
     public List<Patient> getAllPatients(){
         return patientService.getAllPatients();
     }
