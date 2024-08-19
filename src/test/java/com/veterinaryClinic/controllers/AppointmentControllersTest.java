2623a9aa5520774401e587744f08f7fb88f5bbc5
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-08-19 12:48:51 +0200

    test appointment

--- src/test/java/com/veterinaryClinic/controllers/AppointmentControllersTest.java
+++ src/test/java/com/veterinaryClinic/controllers/AppointmentControllersTest.java
@@ -10,11 +10,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static javax.swing.UIManager.get;
 import static org.mockito.ArgumentMatchers.any;
@@ -24,57 +27,62 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AppointmentControllersTest {
-    @Mock private AppointmentServices appointmentServices;
+  @Mock private AppointmentServices appointmentServices;
 
-    @InjectMocks private AppointmentControllers appointmentControllers;
+  @InjectMocks private AppointmentControllers appointmentControllers;
 
-    private MockMvc mockMvc;
+  private MockMvc mockMvc;
 
-    private Appointment appointmentDuque;
-    private Appointment appointmentKoda;
-    private ArrayList<Appointment> appointmentList =  new ArrayList<>();
+  private Appointment appointmentDuque;
+  private Appointment appointmentKoda;
+  private ArrayList<Appointment> appointmentList = new ArrayList<>();
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(appointmentControllers).build();
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+    mockMvc = MockMvcBuilders.standaloneSetup(appointmentControllers).build();
 
-        Patient patientDuque = new Patient();
-        patientDuque.setName("Duque");
-        Patient patientKoda = new Patient();
-        patientKoda.setName("Koda");
+    Patient patientDuque = new Patient();
+    patientDuque.setName("Duque");
+    Patient patientKoda = new Patient();
+    patientKoda.setName("Koda");
 
-        appointmentDuque = new Appointment();
-        appointmentDuque.setId(1);
-        appointmentDuque.setDate(LocalDate.of(2024, 10, 10));
-        appointmentDuque.setTime(LocalTime.of(19, 30));
-        appointmentDuque.setPatient(patientDuque);
-        appointmentDuque.setEmergency(false);
-        appointmentDuque.setReason("lele pancha");
-        appointmentDuque.setPast(false);
-        appointmentDuque.setTreatment("Ibuprofeno");
+    appointmentDuque = new Appointment();
+    appointmentDuque.setId(1);
+    appointmentDuque.setDate(LocalDate.of(2024, 10, 10));
+    appointmentDuque.setTime(LocalTime.of(19, 30));
+    appointmentDuque.setPatient(patientDuque);
+    appointmentDuque.setEmergency(false);
+    appointmentDuque.setReason("lele pancha");
+    appointmentDuque.setPast(false);
+    appointmentDuque.setTreatment("Ibuprofeno");
 
-        appointmentKoda = new Appointment();
-        appointmentKoda.setId(2);
-        appointmentKoda.setDate(LocalDate.of(2024, 04, 29));
-        appointmentKoda.setTime(LocalTime.of(12, 10));
-        appointmentKoda.setPatient(patientKoda);
-        appointmentKoda.setEmergency(true);
-        appointmentKoda.setReason("caca explosiva");
-        appointmentKoda.setPast(true);
-        appointmentKoda.setTreatment("Aspirina");
+    appointmentKoda = new Appointment();
+    appointmentKoda.setId(2);
+    appointmentKoda.setDate(LocalDate.of(2024, 04, 29));
+    appointmentKoda.setTime(LocalTime.of(12, 10));
+    appointmentKoda.setPatient(patientKoda);
+    appointmentKoda.setEmergency(true);
+    appointmentKoda.setReason("caca explosiva");
+    appointmentKoda.setPast(true);
+    appointmentKoda.setTreatment("Aspirina");
 
-        appointmentList.add(appointmentDuque);
-        appointmentList.add(appointmentKoda);
-    }
-
+    appointmentList.add(appointmentDuque);
+    appointmentList.add(appointmentKoda);
+  }
 
   @Test
   void createAppointment() throws Exception {
     when(appointmentServices.createAppointment(any(Appointment.class)))
         .thenReturn(appointmentDuque);
     String appointmentJson =
-        "{'date': '10-10-2024', 'time': '19:30', 'patient': 'patient_id:02','emergency': false,'reason': 'lele pancha','past': false,'treatment': 'Ibuprofeno'}";
+        "{\"id\": 1,\n"
+            + "        \"date\": \"10-10-2024\",\n"
+            + "        \"time\": \"19:30\",\n"
+            + "        \"emergency\": false,\n"
+            + "        \"reason\": \"lele pancha\",\n"
+            + "        \"past\": false,\n"
+            + "        \"treatment\": \"Ibuprofeno\"}";
     mockMvc
         .perform(
             post("/api/vc/appointment")
@@ -84,16 +92,46 @@ public class AppointmentControllersTest {
         .andExpect(
             content()
                 .json(
-                    "{'id':'1',date': '10-10-2024', 'time': '19:30', 'patient': 'patient_id:02','emergency': false,'reason': 'lele pancha','past': false,'treatment': 'Ibuprofeno'}"));
-       }
+                    "{\"id\": 1,\n"
+                        + "        \"date\": \"10-10-2024\",\n"
+                        + "        \"time\": \"19:30\",\n"
+                        + "        \"emergency\": false,\n"
+                        + "        \"reason\": \"lele pancha\",\n"
+                        + "        \"past\": false,\n"
+                        + "        \"treatment\": \"Ibuprofeno\"}"));
+  }
 
 @Test
     void getAllAppointments() throws Exception{
         when(appointmentServices.getAllAppointment()).thenReturn(appointmentList);
-
-        MockMvc.perform(get("/api/vc/appointment"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/vc/appointment"))
                 .andExpect(status().isOk())
-                .andExpect(content().json())
+                .andExpect(content().json("[{\"id\": 1,\n" +
+                        "        \"date\": \"10-10-2024\",\n" +
+                        "        \"time\": \"19:30\",\n" +
+                        "        \"emergency\": false,\n" +
+                        "        \"reason\": \"lele pancha\",\n" +
+                        "        \"past\": false,\n" +
+                        "        \"treatment\": \"Ibuprofeno\"},{\"id\": 2,\n" +
+                        "        \"date\": \"29-04-2024\",\n" +
+                        "        \"time\": \"12:10\",\n" +
+                        "        \"emergency\": true,\n" +
+                        "        \"reason\": \"caca explosiva\",\n" +
+                        "        \"past\": true,\n" +
+                        "        \"treatment\": \"Aspirina\"}]"));
+}
+@Test
+  void getAppointmentId() throws Exception {
+    when(appointmentServices.getAppointmentId(2)).thenReturn(Optional.ofNullable(appointmentKoda));
+    mockMvc.perform(MockMvcRequestBuilders.get("/api/vc/appointment/2"))
+            .andExpect(status().isOk())
+            .andExpect(content().json("{\"id\": 2,\n" +
+                    "                        \"date\": \"29-04-2024\",\n" +
+                    "                        \"time\": \"12:10\",\n" +
+                    "                        \"emergency\": true,\n" +
+                    "                        \"reason\": \"caca explosiva\",\n" +
+                    "                        \"past\": true,\n" +
+                    "                        \"treatment\": \"Aspirina\"}"));
 }
    }
 
